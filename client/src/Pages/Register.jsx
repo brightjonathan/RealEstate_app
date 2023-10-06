93fed6633bf2ec0d23b803719f3b0799fc401353
Bright Jonathan <brightjonathan000@gmail.com>
2023-10-06 13:58:31 +0100

    added the sign-up UI

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>
-      Register page 
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+    <form className='flex flex-col gap-4'>
+      <input
+        type='text'
+        required
+        placeholder='username'
+        className='border p-3 rounded-lg'
+        id='username'
+      />
+      <input
+        type='email'
+        required
+        placeholder='email'
+        className='border p-3 rounded-lg'
+        id='email'
+      />
+      <input
+        type='password'
+        required
+        placeholder='password'
+        className='border p-3 rounded-lg'
+        id='password'
+      />
+
+      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+         Sign Up
+      </button>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Have an account?</p>
+      <Link to={'/sign-in'}>
+        <span className='text-blue-700'> Sign in </span>
+      </Link>
     </div>
+    
+  </div>
   )
 }
 
-export default Register
+export default Register;
