aada9e7342db159b09382f0797379af95ba6fd34
Bright Jonathan <brightjonathan000@gmail.com>
2023-10-06 23:45:19 +0100

    added the Redux state management config setting

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
+import {store} from './Redux/Store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
-    <App/>
+        <App/>
     </Router>
-  </React.StrictMode>,
-)
+  </Provider>,
+);
+
+
