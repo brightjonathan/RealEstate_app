8e3040a17739dc776b103fef7b5b6325586db67d
Bright Jonathan <brightjonathan000@gmail.com>
2023-10-16 19:46:43 +0100

    added the GET listing functionality on the client side

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -222,6 +222,21 @@ const {currentUser, loading, error} = useSelector(state => state.user); //gettin
   };
 
 
+  //show listing functionality by user 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -279,9 +294,46 @@ const {currentUser, loading, error} = useSelector(state => state.user); //gettin
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'> {updateSuccess ? 'User is updated successfully!' : ''} </p>
+
+      <button onClick={handleShowListings} className='text-green-700 w-full'> show listings </button>
+      <p className='text-red-700 mt-5'> {showListingsError ? 'Error showing listings' : ''} </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+               to={`/listing/${listing._id}`}> <p>{listing.title}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase'> Delete </button>
+                <Link to={`#`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
         
     </div>
   )
 }
 
 export default Profile;
+
+
